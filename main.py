3bc9f10bfd24724570c2c61cf1aa237416adada9
yz674 <yz674@duke.edu>
2023-09-10 21:47:00 -0400

    add data visualization

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def data_filter(df):
     return df[df[" \"Test3\""] >= 80]
 
 
 if __name__ == "__main__":
-    print(data_filter(pd.read_csv("./grades.csv")))
+    dataframe = pd.read_csv("./grades.csv")
+    print(data_filter(dataframe))
+    plt.scatter(dataframe["Last name"], dataframe[" \"Test3\""])
+    plt.savefig("data_visualization.png")
